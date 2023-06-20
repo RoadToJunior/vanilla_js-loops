260a46565e06ffcb171e00978f06418d440c1bac
NowWeAre-Free <kontodarm@wp.pl>
2023-06-20 20:12:11 +0200

    exe 2

--- app.js
+++ app.js
@@ -11,3 +11,31 @@ for (let i = 1; i < 101; i++) {
 }
 
 console.log(sum);
+
+// Zadanie 2: Liczba gwiazdek
+
+// Napisz kod, który generuje i wyświetla w konsoli trójkąt prostokątny o wysokości "h" składający się z gwiazdek (*).
+// Użyj pętli for do iteracji po kolejnych poziomach trójkąta, a następnie pętli for do generowania odpowiedniej liczby gwiazdek w każdym poziomie
+// (wydrukuj w konsoli). Przyjmij wartość h równą 6.
+
+// Taki efekt chcemy uzyskać:
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// *****
+
+// ******
+
+for (let i = 1; i <= 6; i++) {
+  let stars = "";
+  for (let j = 1; j <= i; j++) {
+    stars += "*";
+  }
+  console.log(stars);
+}
